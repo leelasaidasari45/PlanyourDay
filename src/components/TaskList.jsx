8ca4afcb5433282a22a12b08ceeeeb22001053aa
SaiPrasad <leelasaidasari@gmail.com>
2026-04-25 09:51:28 +0530

    Fixed bug: destructured completeWithProof in TaskList to enable proof uploads

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -14,6 +14,7 @@ export default function TaskList({ displayDate, date, taskData }) {
     error,
     addTask,
     toggleTask,
+    completeWithProof,
     updateTask,
     deleteTask,
     completedCount,
